dae31a354d7be999c76ecd885146ac0f5fcdcec7
Piotr Husiatyński <phusiatynski@gmail.com>
2013-12-11 19:17:15 +0100

    format post search result with markdown

--- overmind/forum/templatetags/search.py
+++ overmind/forum/templatetags/search.py
@@ -1,14 +1,16 @@
 import re
 
 from django import template
+from django.utils.safestring import mark_safe
 
 
 register = template.Library()
 
 
 @register.simple_tag
-def mark_pattern(text, pattern, fmt=r'<span class="marked-pattern">\1</span>'):
+@register.filter
+def mark_pattern(text, pattern, fmt=r'**\1**'):
     if pattern:
         rx = r"({})".format(re.escape(pattern))
         text = re.sub(rx, fmt, text, flags=re.IGNORECASE)
-    return text
+    return mark_safe(text)
